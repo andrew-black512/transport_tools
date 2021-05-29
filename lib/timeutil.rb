f5d9316387045293b41c4cb631bb4758096fc8ed
Andrew Black <andrew@black1.org.uk>
2021-05-29 14:47:23 +0100

    add timediff

--- lib/timeutil.rb
+++ lib/timeutil.rb
@@ -3,8 +3,7 @@ def addmins(timestr, mins)
     time = Time.parse( timestr) + mins*60
     time.strftime("%H:%M")
 end
-
-#----- -7 timestr
-#pp addmins "11:05",3
-#pp addmins "11:05", -7
-#pp addmins "23:59",3
+def timediff(timestr, timestr_b)
+    timediff = Time.parse( timestr) -  Time.parse( timestr_b)
+    (timediff / 60 ).to_int
+end
