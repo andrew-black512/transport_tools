d98d53a39da2aa8e97e25db65ad352a028ef94f4
Andrew Black <andrew@black1.org.uk>
2021-06-07 13:44:11 +0100

    timefromno

--- lib/timeutil.rb
+++ lib/timeutil.rb
@@ -7,3 +7,9 @@ def timediff(timestr, timestr_b)
     timediff = Time.parse( timestr) -  Time.parse( timestr_b)
     (timediff / 60 ).to_int
 end
+def timefromnow(timestr)
+    timediff = (  Time.parse( timestr) -  Time.now ).to_int
+    sprintf "%2dm% 0.2ds" , timediff / 60 , timediff % 60
+end
+
+#puts timefromnow( '13:55')
