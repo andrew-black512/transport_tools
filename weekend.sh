2eca36501c3e270bf987eedfe5674380db5475a3
Andrew Black <andrew@black1.org.uk>
2021-06-07 18:14:59 +0100

    simlify weekend
    (only useful trains)

--- weekend.sh
+++ weekend.sh
@@ -1,12 +1,3 @@
-OPTIONS='--no-weekdays --format=g -n15'  
-./get_eng.rb EDW:TUH  EDW:LBG  $OPTIONS  | tee 	 extract/weekend_edw.txt
-./get_eng.rb PMR:BFR PMR:VIC PMR:BMS  PMR:ZCW PMR:CLJ  $OPTIONS  | tee 	 extract/weekend_pmr.txt
-
-# a feature -  CLJ:WWO only gives you half the story becuase of "the train will divide"
-./get_eng.rb HHE:WWO BTN:WWO WWO:BAA $OPTIONS  | tee 	 extract/weekend_worthing.txt
-exit
-
-
-
-
-
+OPTIONS='--no-weekend --format=d --time=12:00 -n21'
+#./train_times.rb EDW:TUH  EDW:LBG  $OPTIONS  | tee 	 extract/weekend_edw.txt
+./train_times.rb PMR:BFR PMR:VIC PMR:BMS  PMR:ZCW PMR:CLJ  $OPTIONS  | tee 	 extract/weekend_pmr.txt
