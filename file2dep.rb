1bfac4c6050ce3a5b85486e7d220d04fc0bc0e4a
Andrew Black <andrew@black1.org.uk>
2021-07-07 17:17:27 +0100

    print departures

--- file2dep.rb
+++ file2dep.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby
 
 require 'json'
+require_relative 'printdep'
 
 
 ARGV.each do |f|
    puts "file #{f}"
    data = File.read f
-   trasport_data = JSON::parse( data )
-   pp trasport_data
+   trasport_data = JSON.parse( data )
+   print_depatures trasport_data
+
 
 
 end
