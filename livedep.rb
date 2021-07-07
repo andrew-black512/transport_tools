442a73c5a1e6a213ec0496167393eed89cd2cf28
Andrew Black <andrew@black1.org.uk>
2021-07-07 10:55:16 +0100

    first pass of train

--- livedep.rb
+++ livedep.rb
@@ -6,6 +6,8 @@ require 'json'
 require 'time'
 require 'httparty'
 require 'pp'
+
+require_relative 'printdep.rb'
 require_relative "cred" # TODO: make option
 require_relative 'lib/timeutil'
 
@@ -34,44 +36,7 @@ def getDepartures( stopnum )
 
   return trasport_data
 end
-def get_dep_time ( d )
-  case
-  when d[ 'expected_departure_time' ]
-    d[ 'expected_departure_time' ]
-  when d[ 'aimed_departure_time' ]
-    d[ 'aimed_departure_time' ]
-  else
-    ''
-  end
-end
-###     sprintf "%-5s t%-3s ",
-#      d[ 'expected_departure_time' ] ,
-#      d[ 'aimed_departure_time' ] [3,5]
 
-
-def print_depatures (dep)
-  puts stop_name( dep )
-  wanted_bus = { } ## TODO: make a param
-  wanted_bus = {'185'=> true, '176'=> true ,'484'=> true} ## TODO: make a param
-#  wanted_bus.default = true
-  # puts  dep.keys.join ','
-  dep["departures"].each do |key,depgroup|
-    if wanted_bus [key]
-      puts key    #  a line (normaly a busnumber)
-      #pp depgroup #  an [array] of departures
-      depgroup.each do  |d|
-        dep_time = get_dep_time (d) ###["expected_departure_time"] # TODO
-        wait = timefromnow( dep_time )
-        printf ("    %-6s %-6s (%-5s) %-30s %-10s\n") ,
-           d[ 'expected_departure_time' ] ,
-           d[ 'aimed_departure_time' ] ,
-           wait ,
-           d['direction'] , d['operator']
-      end
-      puts  # blank line between groups
-    end
-  end
-end
 
 def get_stopname( textname) # TODO: rename
   # '490006526A'
