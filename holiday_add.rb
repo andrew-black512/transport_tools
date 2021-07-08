081e61b53ae89929e1ca9b3545e1182d3c1220c9
Andrew Black <andrew@black1.org.uk>
2021-07-08 23:39:26 +0100

    add a TODO (bitmask)

--- holiday_add.rb
+++ holiday_add.rb
@@ -8,6 +8,10 @@ class Date
     #   https://github.com/holidays/holidays#extending-rubys-date-and-time-classes
     #self.holiday?(region) ||
 
+    #TODO a more flexible way is a bitmap
+    #   Weekdays : 02 04 08 10 20
+    #   Weelend  : Sat=40, Sun=01
+
     self.wday == 6 || self.wday == 0 ||
     self.wday == 3 || self.wday == 5   # TODO temporary bodge for Lesley
 
