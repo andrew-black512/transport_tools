0046c6f3df50e200d4ef0b5b942148a85267f088
Andrew Black <andrew@black1.org.uk>
2021-07-08 23:38:32 +0100

    poss line end changes

--- holiday_add.rb
+++ holiday_add.rb
@@ -3,13 +3,14 @@ require "holidays"
 class Date
 
   def nonworkday?(region)
-    #TODO gives undefined method `holiday?' 
-    # see for whetehr it has changed 
+    #TODO gives undefined method `holiday?'
+    # see for whetehr it has changed
     #   https://github.com/holidays/holidays#extending-rubys-date-and-time-classes
-    #self.holiday?(region) || 
+    #self.holiday?(region) ||
 
-    self.wday == 6 || self.wday == 0  
-    
+    self.wday == 6 || self.wday == 0 ||
+    self.wday == 3 || self.wday == 5   # TODO temporary bodge for Lesley
+
   end
 end
 
