40612ba5354d887d2d71a4f10dc641a74e87a320
Andrew Black <andrew@black1.org.uk>
2021-09-01 11:13:58 +0100

    nclude CRS code

--- station_finder.rb
+++ station_finder.rb
@@ -4,13 +4,15 @@ require 'pry'
 require 'pp'
 require 'csv'
 
-
+def datafilename (suffixname )
+	File.join __dir__, suffixname
+end
 def find_station ( word )
-
-  CSV.foreach(  '/home/andrew/dev/transport/station_codes.csv', headers:true ) do |row|
+    datafile = datafilename 'data/station_codes.csv'
+	  CSV.foreach(  datafile, headers:true ) do |row|
 	  station = row[0]
-		if station.match word
-			#binding.pry
+		crs_code = row[1]
+		if station.match(word) or crs_code.match(word)
 			puts row
 		end
   end
