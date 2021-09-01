2c56be19bec6841cc1608c3cf27cd2b4291805b7
Andrew Black <andrew@black1.org.uk>
2021-09-01 10:54:23 +0100

    bascially prints

--- station_finder.rb
+++ station_finder.rb
@@ -6,19 +6,17 @@ require 'csv'
 
 
 def find_station ( word )
-CSV.foreach(  '/home/andrew/dev/transport/station_codes.csv' ) do |row|
-	station = row[0]
-	#binding.pry
-	if station.match word
-		binding.pry
-		puts row
-	end
-	exit
 
-end
+  CSV.foreach(  '/home/andrew/dev/transport/station_codes.csv', headers:true ) do |row|
+	  station = row[0]
+		if station.match word
+			#binding.pry
+			puts row
+		end
+  end
 end
 
-wanted = 'Lond' # ARGV.shift
+wanted = ARGV.shift
 wanted_re = Regexp.new wanted
 
 find_station wanted_re
