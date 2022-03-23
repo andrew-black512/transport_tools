99033c6cfcdb51e588b85cd6ffc510817aeb3e30
Andrew Black <andrew@black1.org.uk>
2022-03-23 16:08:42 +0000

    blank line + layout

--- ttweb.rb
+++ ttweb.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/ruby
-
+# TODO: consider selecting type
 
 ARGV.each do |statpair|
   (from,to)=statpair.split ':'
-
-puts "https://traintimes.org.uk/#{from}/#{to}"
-puts "https://www.realtimetrains.co.uk/search/detailed/gb-nr:#{from}/to/gb-nr:#{to}"
+  puts
+  puts "https://traintimes.org.uk/#{from}/#{to}"
+  puts "https://www.realtimetrains.co.uk/search/detailed/gb-nr:#{from}/to/gb-nr:#{to}"
 end
+puts
