df5a812b2b1cd294b1533b79b16ad3f77b8b99ce
Andrew Black <andrew@black1.org.uk>
2022-03-23 12:09:34 +0000

    busexp: add 37 to banted bus

--- busexp.rb
+++ busexp.rb
@@ -52,7 +52,7 @@ end
 def print_depatures (dep)
   puts stop_name( dep )
   wanted_bus = { } ## TODO: make a param
-  wanted_bus = {'185'=> true, '176'=> true ,'484'=> true} ## TODO: make a param
+  wanted_bus = {'185'=> true, '176'=> true ,'484'=> true, '37'=>true} ## TODO: make a param
 #  wanted_bus.default = true
   # puts  dep.keys.join ','
   dep["departures"].each do |key,depgroup|
