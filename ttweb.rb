d1fe059ac14ea79bc2fd4fe9badacc394124f299
Andrew Black <andrew@black1.org.uk>
2022-03-25 17:00:09 +0000

    Sum: Add nationalrail and some help info

--- ttweb.rb
+++ ttweb.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/ruby
-# TODO: consider selecting type
+# TODO: consider selecting site(NRE, rtt ...) and going to browser.
 
+puts
+puts "Right click on URL to browse"
 ARGV.each do |statpair|
   (from,to)=statpair.split ':'
   puts
   puts "https://traintimes.org.uk/#{from}/#{to}"
   puts "https://www.realtimetrains.co.uk/search/detailed/gb-nr:#{from}/to/gb-nr:#{to}"
+  puts "https://ojp.nationalrail.co.uk/service/ldbboard/dep/#{from}/#{to}/To"
 end
 puts
