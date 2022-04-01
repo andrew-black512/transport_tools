0a53c7445289393f59cac1a2c89f63bf7e6057e4
Andrew Black <andrew@black1.org.uk>
2022-04-01 21:27:58 +0100

    spacing change

--- journey.rb
+++ journey.rb
@@ -1,5 +1,5 @@
 class Journey
-  attr :stat_list    # array of 2 stations 
+  attr :stat_list    # array of 2 stations
   attr :display_stations # array of stations to print
   def initialize( stat_list )
      @stat_list = stat_list.map { |s| s.upcase }
@@ -7,12 +7,12 @@ class Journey
   def route_stations
     special_stations = @stat_list .grep( /[+-]/ ).map{ |t| t.delete('+-')  }
     case special_stations.count
-      when 0 
-        route = [ @stat_list.first, @stat_list.last ] 
-      when 1 
-        route = [ @stat_list.first, special_stations.first ] 
-      when 2 
-        route = [ special_stations.first, special_stations.last ] 
+      when 0
+        route = [ @stat_list.first, @stat_list.last ]
+      when 1
+        route = [ @stat_list.first, special_stations.first ]
+      when 2
+        route = [ special_stations.first, special_stations.last ]
     end
     #TODO check for two stations are the same..... (produces a nonsensal error message!)
     route
