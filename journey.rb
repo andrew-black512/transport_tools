76cfb3f665c1699924dc311defbc0222c51c53a0
Andrew Black <andrew@black1.org.uk>
2022-04-01 21:29:04 +0100

    removed unused code

--- journey.rb
+++ journey.rb
@@ -14,25 +14,21 @@ class Journey
       when 2
         route = [ special_stations.first, special_stations.last ]
     end
-    #TODO check for two stations are the same..... (produces a nonsensal error message!)
-    route
+    route   # array of 2 stations
   end
-  #NOTE this isnt used. Looked cleaner but the array approach makes is easier to reverse the diection
-  def start_station
-     route_stations.first
-  end
+
   def display_stations
      #TODO remove - ones from the list.
      @stat_list.map{ |t| t.delete('+-')  }
-  end 
-  def printinfo 
+  end
+
+  def printinfo
      puts ''
-     puts @route 
-     puts "Route_stations: " + self.route_stations.join(' ') +  " - " +   
-       self.start_station
+     puts @route
+     puts "Route_stations: " + self.route_stations.join(' ')
      puts "Display_stations: " + self.display_stations.join(' ')
   end
 
 
-  
+
 end
