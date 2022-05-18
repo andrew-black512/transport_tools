0281b3af5ce3c7d8c213e57fecd1d0381cf141e3
Andrew Black <andrew@black1.org.uk>
2022-05-18 17:42:19 +0100

    convert from time to date (was incrementing in secs rather than days)

--- lib/human_time.rb
+++ lib/human_time.rb
@@ -1,14 +1,16 @@
 require 'chronic'
 
-def human_time( string )
+## TODO: maybe create a separate _time version
+def human_date( string )
   transform = {
     'tom' => 'tomorrow'  , # shades of VMS
   }
   if s=transform[ string ]
     string = s
-    puts "using #{s}"
+    #puts "using #{s}"
   #when /^+(\d)/ # # TODO: add +dayd
   end
-  Chronic.parse(string, :endian_precedence => :little)
+  time = Chronic.parse(string, :endian_precedence => :little)
     # TODO || raise "bad format#{s}"
+  return time.to_date
 end
