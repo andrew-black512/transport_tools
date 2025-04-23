20916f778c9011b85482bd7982130266c454d658
Andrew Black <andrew@black1.org.uk>
2025-04-23 20:44:15 +0100

    comment added

--- weekend.sh
+++ weekend.sh
@@ -1,3 +1,4 @@
 OPTIONS='--no-weekend --format=d --time=12:00 -n21'
 #./train_times.rb EDW:TUH  EDW:LBG  $OPTIONS  | tee 	 extract/weekend_edw.txt
 ./train_times.rb PMR:BFR PMR:VIC PMR:BMS  PMR:ZCW PMR:CLJ  $OPTIONS  | tee 	 extract/weekend_pmr.txt
+# add comment for test
